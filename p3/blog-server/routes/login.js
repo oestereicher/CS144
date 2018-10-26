a40d7ddca42a5a9c3f8d2a5f59732c3cd18c3e5b
oestereicher <brandon.oestereicher@gmail.com>
2018-10-25 23:15:16 -0700

    started some of the bcrypt

--- p3/blog-server/routes/login.js
+++ p3/blog-server/routes/login.js
@@ -17,18 +17,54 @@ router.get('/', (req, res) => {
     res.render('login', {});
 });
 
+
+//note: i think there's an issue bc the mongodb stuff is nonblocking
+//and somehow we gotta write a callback function so that stuff doesn't
+//happen in the wrong order
+//https://stackoverflow.com/questions/12030248/what-is-the-right-way-to-make-a-synchronous-mongodb-query-in-node-js
 router.post('/', (req, res, next) => {
-    console.log(req.body.username);
-    console.log(req.body.password);
+    let user = req.body.username;
+    let pass = req.body.password;
     //checking to make sure that a username and password were entered
     //unsure if this is correct
-   if (!req.body.username || !req.body.password) {
+   if (!user || !pass) {
        next(createError(400));
        //res.render('login', {});
    }
    else {
-       // in progress: bcrypt.hash(req.body.password)
-       res.render('login', {});
+       let hashword = new Array();
+       MongoClient.connect(url, function(err, client) {
+           assert.equal(null, err);
+           console.log("Connected correctly to server in blog.js");
+           let db = client.db(dbName);
+           let query = {'username': user};
+           db.collection("Users").find(query, {
+               projection: {_id: 0, username: 0}
+           }).toArray(function(err, result) {
+               if (err) throw err;
+               console.log(result);
+               hashword = result;
+               client.close();
+           });
+       });
+       console.log("terrible terrible" + hashword);
+       if (hashword.length != 1) { //user not found
+           next(createError(401));
+       }
+       else {
+           bcrypt.compare(pass, hashword[0].password, function (err, res) {
+               assert.equal(null, err);
+               if (!res) { //password does not match database
+                   console.log("terrible horrible or just mistake");
+                   next(createError(401));
+               }
+               else {
+                   //good good, gotta check the redirect stuff
+                   console.log("yay, good password!!!1");
+               }
+           })
+       }
+       //res.render('login', {});
    }
 });
 
