fe2de816af4cdbbe86e2303add00bd92a0805c9e
oestereicher <brandon.oestereicher@gmail.com>
2018-10-25 22:40:44 -0700

    login page displays that's it tho

--- p3/blog-server/app.js
+++ p3/blog-server/app.js
@@ -7,7 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var blogRouter = require('./routes/blog');
+var loginRouter = require('./routes/login')
 
+//don't know if this connection is necessary
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 //connection url
@@ -37,6 +39,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/blog', blogRouter);
+app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- p3/blog-server/routes/login.js
+++ p3/blog-server/routes/login.js
@@ -1,3 +1,4 @@
+var createError = require('http-errors');
 let express = require('express');
 let router = express.Router();
 let app = express();
@@ -6,9 +7,29 @@ let assert = require('assert');
 let url = 'mongodb://localhost:27017';
 let dbName = 'BlogServer';
 
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 app.set('view engine', 'ejs');
 app.set('views', '.');
 
-router.get('/login', (req, res) => {
-
+router.get('/', (req, res) => {
+    res.render('login', {});
 });
+
+router.post('/', (req, res, next) => {
+    console.log(req.body.username);
+    console.log(req.body.password);
+    //checking to make sure that a username and password were entered
+    //unsure if this is correct
+   if (!req.body.username || !req.body.password) {
+       next(createError(400));
+       //res.render('login', {});
+   }
+   else {
+       // in progress: bcrypt.hash(req.body.password)
+       res.render('login', {});
+   }
+});
+
+module.exports = router;
