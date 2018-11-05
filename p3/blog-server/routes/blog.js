cb24741cb23b040c742132609e057b876e3647d6
kristi <kristi5327@gmail.com>
2018-11-04 21:14:29 -0800

    FINALLLY with unordered post nums

--- p3/blog-server/routes/blog.js
+++ p3/blog-server/routes/blog.js
@@ -21,6 +21,7 @@ router.get('/:username', (req, res) => {
         db.collection("Posts").find(query).toArray(function(err, result) {
             if (err) throw err;
             console.log(result);
+            if(result.length == 0)
             posts = result;
             let renderObj = new Object();
             renderObj.title = "Posts";
@@ -43,7 +44,41 @@ router.get('/:username', (req, res) => {
 
 });
 
-/* I don't think this is necessary
+
+router.get('/:username/:postid', (req, res) => {
+	console.log(req.params.username);
+	console.log(req.query.start);
+    MongoClient.connect(url, function(err, client) {
+        assert.equal(null, err);
+        console.log("Connected correctly to server in blog.js");
+        let db = client.db(dbName);
+        //let query = { $and:[{'username': req.params.username}, {'postid':req.params.postid}] };
+        let query = {'username': req.params.username, 'postid':parseInt(req.params.postid)};
+        console.log("after query");
+        db.collection("Posts").find(query).toArray(function(err, result) {
+            console.log("reached pre-error");
+            if(result.length == 0)
+                res.status(404).send("Post not found");
+            //if (err) throw err;
+            console.log("after the error thing");
+            console.log(result);
+            posts = result;
+            let renderObj = new Object();
+            renderObj.title = "Posts";
+            renderObj.postTitles = new Array();
+            renderObj.posts = new Array();
+
+            renderObj.postTitles.push(posts[0].title);
+            renderObj.posts.push(posts[0].body);
+
+            res.render('blog', renderObj);
+            client.close();
+        });
+    });
+
+});
+
+/* I don't think this is necessary -- OG stuff 
 router.get('/:username/:postid', (req, res) => {
     let renderObj = new Object();
     renderObj.title = "Posts";
