78dfbe65d2407c5afecaee5415af486e210d27f5
kristi <kristi5327@gmail.com>
2018-11-24 15:41:45 -0800

    i guess list works

--- p4/angular-blog/src/app/blog.service.ts
+++ p4/angular-blog/src/app/blog.service.ts
@@ -1,67 +1,135 @@
+import {FAKEPOSTS} from "./fake-posts";
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import {catchError, tap} from "rxjs/operators";
 
-const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-};
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlogService {
-  private posts: Observable<Post[]>;
-  private http: HttpClient;
-  private api: string = "/api/";
+  private posts: Post[] = [];
+  private nextID: number;
+  private api: string = "http://localhost:3000/api/";
 
   constructor() { }
 
   fetchPosts(username: string): void {
-    this.posts = this.http.get<Post[]>(this.api + username)
-      .pipe(
-        tap(catchError(this.handleError('fetchPosts', [])))
-      );
+    let httpReq = new XMLHttpRequest();
+    let classThis = this;
+    httpReq.onreadystatechange = function() {
+      if (httpReq.readyState == XMLHttpRequest.DONE) {
+        let res = JSON.parse(httpReq.responseText);
+        let postnum = 0;
+        while (postnum < res.length) {
+          let post = {
+            postid: res[postnum].postid,
+            created: new Date(res[postnum].created),
+            modified: new Date(res[postnum].modified),
+            title: res[postnum].title,
+            body: res[postnum].body
+          };
+          classThis.posts.push(post);
+          postnum++;
+        }
+      }
+    };
+
+    httpReq.open('GET', this.api + username, true);
+    httpReq.send();
   }
 
-  getPosts(username: string): Observable<Post[]> {
+  getPosts(username: string): Post[] {
+    //console.log("I PRINTED FAKEPOSTS");
+    //console.log(FAKEPOSTS);
+    //return FAKEPOSTS;
     return this.posts;
   }
 
-  getPost(username: string, id: number): Observable<Post> {
-    let postGot: Observable<Post>;
-    this.posts.subscribe( posts => {
-      postGot = posts.find(post => post.postid === id);
-    });
-    return postGot;
+  getPost(username: string, id: number): Post {
+    return this.posts.find(function(post) {
+      return post.postid == id;
+    })
   }
 
-  newPost(username: string): Observable<Post> {
-
-    return this.http.post<Post>(this.api + username)
+  newPost(username: string): Post {
+    let newPost = {
+      postid: this.nextID,
+      created: new Date(),
+      modified: new Date(),
+      title: "",
+      body: ""
+    };
+    this.posts.push(newPost);
+    let httpReq = new XMLHttpRequest();
+    httpReq.onreadystatechange = function() {
+      if (httpReq.readyState == XMLHttpRequest.DONE && httpReq.status == 201) {
+        console.log("good job making new post woot");
+      }
+      else if (httpReq.readyState == XMLHttpRequest.DONE) {
+        window.alert("error creating new post rip lmao");
+        window.location.href = "http://localhost:3000/edit/";
+      }
+    };
+    //TODO: pick a postid to create new post with
+    let postid = 69;
+    httpReq.open("POST", this.api + username + "/" + postid.toString());
+    httpReq.setRequestHeader("Content-type", "application/json");
+    httpReq.send(JSON.stringify(newPost));
+    return newPost;
   }
 
   updatePost(username: string, post: Post): void {
+    let index = this.postToIndex(post.postid);
+    if (index != -1) {
+      this.posts[index].title = post.title;
+      this.posts[index].body = post.body;
+      this.posts[index].modified = new Date();
+      let httpReq = new XMLHttpRequest();
+      httpReq.onreadystatechange = function() {
+        if (httpReq.readyState == XMLHttpRequest.DONE && httpReq.status == 200) {
+          console.log("successful update wooo");
+        }
+        else if (httpReq.readyState == XMLHttpRequest.DONE) {
+          window.alert("error updating post");
+          window.location.href = "http://localhost:3000/edit/" + post.postid;
+        }
+      }
 
+    }
   }
 
   deletePost(username: string, postid: number): void {
+    let index = this.postToIndex(postid);
+    if (index != -1) {
+      let httpReq = new XMLHttpRequest();
+      httpReq.onreadystatechange = function() {
+        if (httpReq.readyState == XMLHttpRequest.DONE && httpReq.status == 204) {
+          console.log("successful delete guud job");
+        }
+        else if (httpReq.readyState == XMLHttpRequest.DONE) {
+          window.alert("error deleting post");
+          window.location.href = "http://localhost:3000/";
+        }
+      };
 
+      this.posts = this.posts.filter(function(post) {
+        return post.postid !== postid;
+      });
+      httpReq.open('DELETE', this.api + username + "/" + postid.toString(), true);
+      httpReq.send();
+    }
   }
 
-  private handleError<T> (operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
 
-      // TODO: send the error to remote logging infrastructure
-      console.error(error); // log to console instead
-
-      // TODO: better job of transforming error for user consumption
-      this.log(`${operation} failed: ${error.message}`);
-
-      // Let the app keep running by returning an empty result.
-      return of(result as T);
-    };
+  postToIndex(id: number): number {
+    for (let i = 0; i < this.posts.length; i++) {
+      if (this.posts[i].postid == id) {
+        return i;
+      }
+    }
+    return -1;
   }
+
+
 }
 
 export class Post {
--- p4/angular-blog/src/app/list/list.component.ts
+++ p4/angular-blog/src/app/list/list.component.ts
@@ -10,17 +10,23 @@ export class ListComponent implements OnInit {
 
   constructor(private blogService: BlogService) { }
   posts: Post[];
+  username: string;
 
   ngOnInit() {
+    this.posts = this.blogService.getPosts(this.username);
+    console.log("INSIDE LIST COMPOMENT PRINTING");
+    console.log(this.posts);
   }
 
   getPosts():void{
-    this.posts = this.blogService.getPosts('CS144');
+    this.posts = this.blogService.getPosts(this.username);
   }
 
  parseJWT(token) :void{
+   token = document.cookie; //WRONG
     let base64Url = token.split('.')[1];
     let base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    return JSON.parse(atob(base64));
+    let decoded = JSON.parse(atob(base64));
+    this.username = decoded.usr;
   }
 }
